777266ffa3c862f316364b48d9420ed2188b852c
Courage Zeng <czeng@lcs.on.ca>
2024-10-03 08:57:20 -0400

    Initial Commit

---
+++ GeometricFigures/GeometricFiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFiguresApp.swift
+//  GeometricFigures
+//
+//  Created by 曾梓恒 on 2024/10/3.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
