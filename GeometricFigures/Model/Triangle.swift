ecd6ad48be7a1c107220353e15f61683e7f091f7
Courage Zeng <czeng@lcs.on.ca>
2024-10-04 11:15:35 -0400

    Added triangle structure to my model, and made a slider that controls and shows the length of sideA.

---
+++ GeometricFigures/Model/Triangle.swift
@@ -0,0 +1,8 @@
+//
+//  Triangle.swift
+//  GeometricFigures
+//
+//  Created by 曾梓恒 on 2024/10/3.
+//
+
+import Foundation
---
+++ GeometricFigures/Views/TriangleView.swift
@@ -0,0 +1,18 @@
+//
+//  TriangleView.swift
+//  GeometricFigures
+//
+//  Created by 曾梓恒 on 2024/10/3.
+//
+
+import SwiftUI
+
+struct TriangleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TriangleView()
+}
