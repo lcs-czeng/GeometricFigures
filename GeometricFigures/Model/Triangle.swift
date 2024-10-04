3c98f56c3be5ae4c93e2412044d10d5d139bacbe
Courage Zeng <czeng@lcs.on.ca>
2024-10-04 11:17:51 -0400

    Added my triangle structure to the model, and made a slider that controls and shows the length of sideA in a triangle.

--- GeometricFigures/Model/Triangle.swift
+++ GeometricFigures/Model/Triangle.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+protocol Describable {
+    var description: String { get }
+}
+
+struct Triangle: Describable {
+    
+    //Stored properties
+    var sideA: Double
+    var sideB: Double
+    var sideC: Double
+    var heightOfTriangle: Double
+    
+    //Computed properties
+    var perimeter: Double {
+        return sideA + sideB + sideC
+    }
+    
+    var area: Double {
+        return sideB * heightOfTriangle * 0.5
+    }
+    
+    var description: String {
+        return "The heightOfTriangle of this triangle is \(heightOfTriangle) units."
+    }
+
+}
--- GeometricFigures/Views/TriangleView.swift
+++ GeometricFigures/Views/TriangleView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct TriangleView: View {
+    
+    //MARK: Stored properties
+    
+    @State var currentTriangle = Triangle(
+        sideA: 5.0,
+        sideB: 8.0,
+        sideC: 5.0,
+        heightOfTriangle: 3.0
+    )
+    
+    //MARK: Computed properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            //Add an image
+            
+            //Label - describe what the slide is for
+            Text("sideA")
+            //Slider control - to allow user input
+            Slider(
+                value: $currentTriangle.sideA,
+                in: 1...100,
+                step: 1.0
+            )
+            //Label - show current slider value
+            Text("sideA is \(currentTriangle.sideA.formatted())")
+            
+        }
     }
 }
 
